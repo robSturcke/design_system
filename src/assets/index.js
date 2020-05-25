c4a60b07fe14ef480642d9a67850323a83e705c1
robSturcke <rob@rslabs.dev>
2020-05-25 19:03:23 -0400

    design system example

---
+++ src/assets/index.js
@@ -0,0 +1,9 @@
+import SignUp from './illustrations/SignUp.svg';
+import SignIn from './illustrations/SignIn.svg';
+
+export * from './icons/CloseIcon';
+
+export const Illustrations = {
+  SignUp,
+  SignIn,
+};
--- src/index.js
+++ src/index.js
@@ -1,17 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { ThemeProvider } from 'styled-components';
+import { SignUpModal } from './components';
+import { GlobalStyle, defaultTheme, darkTheme } from './utils';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const App = () => {
+  const [showModal, setShowModal] = useState(false);
+  const [useDarkTheme, setUseDarkTheme] = useState(false);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+  return (
+    <ThemeProvider theme={useDarkTheme ? darkTheme : defaultTheme}>
+      <button
+        onClick={() => setUseDarkTheme(true)}
+        style={{ margin: '0 16px 24px', padding: '8px', background: 'none' }}
+      >
+        Dark theme
+      </button>
+      <button
+        onClick={() => setUseDarkTheme(false)}
+        style={{ margin: '0 16px 24px', padding: '8px', background: 'none' }}
+      >
+        Default theme
+      </button>
+      <button
+        onClick={() => setShowModal(!showModal)}
+        style={{ margin: '0 16px 24px', padding: '8px', background: 'none' }}
+      >
+        ShowModal
+      </button>
+
+      <div
+        style={{
+          background: useDarkTheme
+            ? defaultTheme.primaryColor
+            : darkTheme.primaryColor,
+          width: '100vw',
+          height: '100vh',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'space-around',
+        }}
+      >
+        <SignUpModal showModal={showModal} setShowModal={setShowModal} />
+      </div>
+      <GlobalStyle />
+    </ThemeProvider>
+  );
+};
+
+ReactDOM.render(<App />, document.querySelector('#root'));
